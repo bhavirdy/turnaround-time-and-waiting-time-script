b3daf4653e39f83784a1f9eb5092a569b75a54bc
Bhavishyan Reddy <102685501+bhavirdy@users.noreply.github.com>
2023-05-15 19:48:48 +0000

    added methods

--- tt-wt-script.py
+++ tt-wt-script.py
@@ -1,35 +1,50 @@
 import statistics
 
-with open('FCFS_10_40.txt', 'r') as file:
+
+def get_average_turnaround_time(file_contents):
+    current_pid = 0
+
+    actual_start_times = []
+    terminated_times = []
+
+    for line in file_contents:
+        pid, state, mode, start_time, end_time, program = line.strip().split(', ')
+
+        pid = int(pid)
+        start_time = int(start_time)
+
+        if pid != current_pid:
+            current_pid = pid
+            actual_start_times.append(start_time)
+        elif state == 'TERMINATED':
+            terminated_times.append(start_time)
+        else:
+            continue
+
+    turnaround_times = []
+    for index, time in enumerate(actual_start_times):
+        turnaround_times.append(terminated_times[index] - time) 
+
+    average_turnaround_time = round(statistics.mean(turnaround_times), 2)
+
+    return average_turnaround_time
+
+def get_average_waiting_time(file_contents):
+
+    average_waiting_time = -1
+
+    return average_waiting_time
+
+
+file_name = 'FCFS_5_20.txt'
+
+with open(file_name, 'r') as file:
     # skip header line
     next(file)
     file_contents = file.readlines()
 
-current_pid = 0
+average_turnaround_time = get_average_turnaround_time(file_contents)
+average_waiting_time = get_average_waiting_time(file_contents)
 
-actual_start_times = []
-terminated_times = []
-
-for line in file_contents:
-    pid, state, mode, start_time, end_time, program = line.strip().split(', ')
-
-    pid = int(pid)
-    start_time = int(start_time)
-
-    if pid != current_pid:
-        current_pid = pid
-        actual_start_times.append(start_time)
-    elif state == 'TERMINATED':
-        terminated_times.append(start_time)
-    else:
-        continue
-
-turnaround_times = []
-for index, time in enumerate(actual_start_times):
-    turnaround_times.append(terminated_times[index] - time)
-
-print(turnaround_times)   
-
-average_turnaround_time = statistics.mean(turnaround_times)
-
-print(round(average_turnaround_time, 2))
+print("Average turnaround time for", file_name, "is:", average_turnaround_time)
+print("Average waiting time for", file_name, " is:", average_waiting_time)
